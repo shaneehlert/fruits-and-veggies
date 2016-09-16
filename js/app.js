5f18c6ba48d67192ba9601e0addd8391c250ac59
shaneehlert <shane@esperas.org>
2016-09-16 09:35:29 -0700

    couldnt figure it out

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /* setup your angular app here */
-
+var FruitVeggie = angular.module('FruitVeggie', []);
 // debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruits.length);
-console.log('Veggie count', vegetables.length);
+FruitVeggie.controller('Ctrl', ['$scope', function($scope, fruit){
+  $scope.fruit = [
+  'Apple',
+  'Apricot',
+  'Banana'
+]
+$scope.submitfruits = function() {
+  $scope.toDoList.push($scope.fruits);
+  $scope.toDo = '';
+};
+}])
